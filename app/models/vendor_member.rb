3d33bce17f136a35994cd7f9f413984b84344fd9
Travis <numbers1311407@gmail.com>
2011-08-26 15:05:26 -0400

    removed old vendor_proxy association method and logo mount

--- app/models/vendor_member.rb
+++ app/models/vendor_member.rb
@@ -1,20 +1,12 @@
 class VendorMember < ActiveRecord::Base
   include E9Vendors::Model
 
+  has_many :vendor_proxies, :dependent => :destroy
+  has_many :vendors, :through => :vendor_proxies
+
+  validates :name, :presence => true
+
   def self.add_vendor_proxy(proxy_association)
     all.each {|member| member.vendor_proxies << proxy_association.build }
   end
-
-  has_many :vendor_proxies, :dependent => :destroy do
-    def by_category
-      includes(:vendor => :vendor_categories)
-        .order('vendor_categories.position')
-        .group_by {|vd| vd.vendor.vendor_category.name }
-    end
-  end
-  has_many :vendors, :through => :vendor_proxies
-
-  mount_uploader :logo, LogoUploader
-
-  validates :name, :presence => true
 end
